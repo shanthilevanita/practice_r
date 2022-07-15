33fddd03f248684a57535b9f68a4ee30d87735ea
shanthilevanita <sha.levanita@gmail.com>
2022-07-15 12:30:40 +0100

    initial commit

---
+++ datafromhabeebah.R
@@ -0,0 +1,27 @@
+# Command or codes  
+sc<- read.csv("dataAnova-1.csv")
+mean(sc$HaemoglobinÂ.)
+min(sc$HaemoglobinÂ.)
+max(sc$HaemoglobinÂ.)
+median(sc$HaemoglobinÂ.)
+
+summary(sc$HaemoglobinÂ.)
+hist(sc$HaemoglobinÂ.)
+
+mean (sc[1:16,2])
+
+
+sc[1:17-27]
+mean(sc[17:26,2])
+mean(sc[27:41,2])
+mean(SC[sc$sickleCellÂ.== "ss", 2])
+mean(sc[sc$ï..sickleCellÂ.=="ss",2])
+mean(sc[sc$ï..sickleCellÂ.=="sb",2])
+mean(sc[sc$ï..sickleCellÂ.=="sc",2])
+mean(SC[sc$sickleCellÂ.== "ss"])
+sc$ï..sickleCellÂ.== "ss"
+
+mean(SC[,2])
+mean(sc[,2])
+
+mean (sc[,"HaemoglobinÂ."])
